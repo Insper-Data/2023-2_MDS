afd112ab54d81ae8c4b1538df2854a468200b32e
gustavo-tm <gtheilm@gmail.com>
2023-11-28 16:41:22 -0300

    entrega 2

--- descritiva.R
+++ descritiva.R
@@ -8,8 +8,79 @@ library(stringi)
 library(openxlsx)
 
 df <- PNADcIBGE::get_pnadc(2022, topic = 4, labels = TRUE, design = TRUE)
+df <- PNADcIBGE::read_pnadc(microdata = "PNADC_032023.txt", 
+                            input_txt = "input_PNADC_trimestral.txt") %>% 
+  pnadc_design()
 
-#Por onde pessoas não ocupadas buscam emprego, separado BF ----
+#Quais ocupaÃ§Ãµes mais empregam----
+ocupacoes_emprego <- df %>% 
+  srvyr::as_survey(strata = stype) %>% 
+  srvyr::group_by(UF, V4010) %>% 
+  srvyr::survey_tally() %>% 
+  drop_na() %>% 
+  mutate(grupo = case_when(startsWith(as.character(V4010), "00000") ~ "OcupaÃ§Ãµes maldefinidas",
+                           startsWith(as.character(V4010), "0") ~ "Membros das forÃ§as armadas, policiais e bombeiros militares",
+                           startsWith(as.character(V4010), "1") ~ "Diretores e gerentes",
+                           startsWith(as.character(V4010), "2") ~ "Profissionais das ciÃªncias e intelectuais",
+                           startsWith(as.character(V4010), "3") ~ "TÃ©cnicos e profissionais de nÃ­vel mÃ©dio",
+                           startsWith(as.character(V4010), "4") ~ "Trabalhadores de apoio administrativo",
+                           startsWith(as.character(V4010), "5") ~ "Trabalhadores dos serviÃ§os, vendedores dos comÃ©rcios e mercados",
+                           startsWith(as.character(V4010), "6") ~ "Trabalhadores qualificados da agropecuÃ¡ria, florestais, da caÃ§a e da pesca",
+                           startsWith(as.character(V4010), "7") ~ "Trabalhadores qualificados, operÃ¡rios e artesÃµes da construÃ§Ã£o, das artes mecÃ¢nicas e outros ofÃ­cios",
+                           startsWith(as.character(V4010), "8") ~ "Operadores de instalaÃ§Ãµes e mÃ¡quinas e montadores",
+                           startsWith(as.character(V4010), "9") ~ "OcupaÃ§Ãµes elementares",
+                           TRUE ~ "number")) %>% 
+  group_by(UF, grupo) %>% 
+  summarize(n = sum(n))
+
+ocupacoes_emprego.tabela <- setores_emprego %>%
+  group_by(UF) %>% 
+  summarize(sum_n = sum(n)) %>% 
+  right_join(setores_emprego) %>% 
+  group_by(UF, grupo) %>% 
+  summarize(sum_n = mean(sum_n),
+            n = sum(n)) %>% 
+  mutate(prop = n/sum_n)
+
+ocupacoes_emprego.tabela %>% 
+  pivot_wider(id_cols = UF, names_from = grupo, values_from = prop) %>% 
+  openxlsx::write.xlsx("tabelas/ocupacoes_emprego.xlsx")
+
+#Quais setores mais empregam ----
+setor_emprego <- df %>% 
+  srvyr::as_survey(strata = stype) %>% 
+  srvyr::group_by(UF, V4013) %>% 
+  srvyr::survey_tally() %>% 
+  drop_na() %>% 
+  mutate(grupo = case_when(startsWith(as.character(V4013), "00000") ~ "Atividades maldefinidas",
+                           V4013 > 01101 & V4013 < 03002 ~ "Agricultura, pecuÃ¡ria, produÃ§Ã£o florestal, pesca e aquicultura",
+                           V4013 > 05000 & V4013 < 39000 ~ "IndÃºstria geral",
+                           V4013 > 41000 & V4013 < 43000 ~ "ConstruÃ§Ã£o",
+                           V4013 > 45010 & V4013 < 48100 ~ "ComÃ©rcio; reparaÃ§Ã£o de veÃ­culos automotores e motocicletas",
+                           V4013 > 49010 & V4013 < 53002 ~ "Transporte, armazenagem e correio",
+                           V4013 > 55000 & V4013 < 56020 ~ "Alojamento e alimentaÃ§Ã£o ",
+                           V4013 > 58000 & V4013 < 82009 ~ "InformaÃ§Ã£o, comunicaÃ§Ã£o e atividades financeiras, imobiliÃ¡rias, profissionais e administrativas ",
+                           V4013 > 84011 & V4013 < 88000 ~ "AdministraÃ§Ã£o pÃºblica, defesa e seguridade social, educaÃ§Ã£o, saÃºde humana e serviÃ§os sociais",
+                           V4013 == 97000 ~ "ServiÃ§os domÃ©sticos",
+                           (V4013 > 90000 & 96090 < 03002) | V4013 == 99000 ~ "Outros serviÃ§os ",
+                           TRUE ~ "number")) %>% 
+  group_by(UF, grupo) %>% 
+  summarize(n = sum(n))
+
+setor_emprego.tabela <- setor_emprego %>%
+  group_by(UF) %>% 
+  summarize(sum_n = sum(n)) %>% 
+  right_join(setor_emprego) %>% 
+  group_by(UF, grupo) %>% 
+  summarize(sum_n = mean(sum_n),
+            n = sum(n)) %>% 
+  mutate(prop = n/sum_n)
+
+setor_emprego.tabela %>% 
+  pivot_wider(id_cols = UF, names_from = grupo, values_from = prop) %>% 
+  openxlsx::write.xlsx("tabelas/setor_emprego.xlsx")
+
+#Por onde pessoas n?o ocupadas buscam emprego, separado BF ----
 busca_emprego <- df %>% 
   srvyr::as_survey(strata = stype) %>% 
   srvyr::group_by(UF, V4072A, VI5002A) %>% 
@@ -24,18 +95,18 @@ busca_emprego.tabela <- busca_emprego %>%
     V4072A,
     "Fez ou inscreveu-se em concurso" = c("Fez ou inscreveu-se em concurso"),
     "Consultou parente, amigo ou colega" = c("Consultou parente, amigo ou colega"),
-    "Tomou medida para iniciar o próprio negócio" = c("Tomou medida para iniciar o próprio negócio (recursos financeiros, local para instalação, equipamentos, legalização etc.)"),
-    "Consultou ou inscreveu-se em agência de emprego" = c("Consultou ou inscreveu-se em agência de emprego municipal, estadual ou no Sistema Nacional de Emprego (SINE)",
-                                                          "Consultou ou inscreveu-se em agência de emprego privada ou sindicato"),
-    "Entrou em contato com empregador" = c("Entrou em contato com empregador (pessoalmente, por telefone, por email ou pelo portal da empresa, inclusive enviando currículo)"),
-    "Não tomou providência / Tomou outra providência" = c("Tomou outra providência, especifique:",
-                                                          "Não tomou providência efetiva"))) %>% 
+    "Tomou medida para iniciar o pr?prio neg?cio" = c("Tomou medida para iniciar o pr?prio neg?cio (recursos financeiros, local para instala??o, equipamentos, legaliza??o etc.)"),
+    "Consultou ou inscreveu-se em ag?ncia de emprego" = c("Consultou ou inscreveu-se em ag?ncia de emprego municipal, estadual ou no Sistema Nacional de Emprego (SINE)",
+                                                          "Consultou ou inscreveu-se em ag?ncia de emprego privada ou sindicato"),
+    "Entrou em contato com empregador" = c("Entrou em contato com empregador (pessoalmente, por telefone, por email ou pelo portal da empresa, inclusive enviando curr?culo)"),
+    "N?o tomou provid?ncia / Tomou outra provid?ncia" = c("Tomou outra provid?ncia, especifique:",
+                                                          "N?o tomou provid?ncia efetiva"))) %>% 
   group_by(UF, VI5002A, V4072A) %>% 
   summarize(sum_n = mean(sum_n),
             n = sum(n)) %>% 
   mutate(prop = n/sum_n) %>% 
   filter(VI5002A != "Ignorado") %>% 
-  mutate(VI5002A = ifelse(VI5002A == "Sim", "Recebe Bolsa Família", "Não Recebe Bolsa Família"),
+  mutate(VI5002A = ifelse(VI5002A == "Sim", "Recebe Bolsa Fam?lia", "N?o Recebe Bolsa Fam?lia"),
          UF = fct_relevel(UF, sort))
 
 busca_emprego.tabela %>% 
@@ -44,7 +115,7 @@ busca_emprego.tabela %>%
   facet_wrap(~ VI5002A) +
   labs(title = "Como as pessoas buscam empregos", 
        x = "Maneiras que as pessoas buscam emprego (%)",
-       y = "Unidade Federativa", fill = "Opções de resposta na PNAD") +
+       y = "Unidade Federativa", fill = "Op??es de resposta na PNAD") +
   theme(legend.position = "bottom", legend.direction="vertical") +
   scale_x_continuous(labels = scales::percent) +
   scale_fill_viridis_d(direction = -1)
@@ -68,10 +139,10 @@ carteira_assinada <- df %>%
   srvyr::survey_tally() %>% 
   pivot_wider(names_from = V4029, values_from = n, id_cols = c(UF, VI5002A)) %>% 
   filter(VI5002A != "Ignorado") %>% 
-  mutate(tx_carteira_assinada = Sim/(Sim + Não),
+  mutate(tx_carteira_assinada = Sim/(Sim + NÃ£o),
          UF = stri_trans_general(str = str_to_title(UF), 
                                       id = "Latin-ASCII"),
-         VI5002A = ifelse(VI5002A == "Sim", "Recebe Bolsa Família", "Não Recebe Bolsa Família")) %>%
+         VI5002A = ifelse(VI5002A == "Sim", "Recebe Bolsa Fam?lia", "N?o Recebe Bolsa Fam?lia")) %>%
   left_join(estados_geometria)
 
 carteira_assinada %>% 
